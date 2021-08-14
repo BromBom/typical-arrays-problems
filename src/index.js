378a607f412b472f5fbabbaff6d96d24f3d589cb
Ihar Levachkov <pokerlis@gmail.com>
2021-08-14 23:52:33 +0300

    feat: creat js code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ((!Array.isArray(array)) || (array.length === 0)) return 0;
+    else {
+        let minItem = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < minItem) {
+                minItem = array[i];
+            }
+        }
+        return minItem;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ((!Array.isArray(array)) || (array.length === 0)) return 0;
+    else {
+        let maxItem = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > maxItem) {
+                maxItem = array[i];
+            }
+        }
+        return maxItem;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ((!Array.isArray(array)) || (array.length === 0)) return 0;
+    else {
+        return array.reduce((a, b) => (a + b)) / array.length;
+    }
 }
